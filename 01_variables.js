542480e97408f1b4ca120ff80ee29ec3a8a4944a
Adityarj1910 <adityarj1910@gmail.com>
2024-03-21 21:37:11 +0530

    data types

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "aditya123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna "
+let accountState
+
+// accountId = 20 // not allowed
+console.log(accountId)
+accountEmail="aditya@gmail.com"
+accountPassword="232"
+accountCity="BLR"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
