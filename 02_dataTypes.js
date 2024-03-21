0d4804003431d44aa619ab6a053e4f60bceb4951
Adityarj1910 <adityarj1910@gmail.com>
2024-03-21 22:44:07 +0530

    introduction to data types

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -1,2 +1,17 @@
 "use strict";
 // alert("hello")
+let name="aditya"
+let age = 21
+let isLoggedIn = true
+
+/*
+number
+bigInt
+string ""
+boolean -> true/false
+null -> standalone value (empty value)
+undefined -> when then value is not defined
+symbol -> unique
+
+object
+*/
---
+++ 03_typeConversion.js
@@ -0,0 +1,34 @@
+// let score = "33abc"
+
+// console.log(typeof (score))
+// let abc = Number(score)
+
+// console.log(abc)
+// console.log(typeof abc);
+
+/*
+"33"=> 33   to number
+"33abc" => NaN
+true => 1   false => 0
+null => 0   undefined => NaN
+*/
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+/* to boolean
+1 => true
+0 => false
+"" => false
+"aditya" => true
+*/
+
+let someNum = 1234
+let str = String(someNum)
+console.log(typeof str)
+console.log(str)
+
+
