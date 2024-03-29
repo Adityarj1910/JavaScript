f8b25de6b14e4b4cdec873f1654694c789ad999e
Adityarj1910 <adityarj1910@gmail.com>
2024-03-29 12:12:46 +0530

    stack heap

--- 05_dataTypes-Summary.js
+++ 05_dataTypes-Summary.js
@@ -1,8 +1,8 @@
 /*
-primitive  => 7 types:
+primitive  => 7 types: (stack memory)
     String, Number, Boolean, null, undefined, Symbol, BigInt
 
-Reference type => non primitive:
+Reference type => non primitive: (heap memorry)
     whose reference can be directly allocted to you
     Array, Objects, Functions
 */
@@ -11,4 +11,18 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 // console.log(id)
 // console.log(anotherId);
-console.log(id===anotherId)
+// console.log(id===anotherId) 
+
+const heros = ["shaktiman", "nagraj", "doga"] // array
+
+let obj = {
+    name: "aditya",
+    age : 21
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction) //function but is object function
+console.log(typeof id)
---
+++ 06_strings.js
@@ -0,0 +1,44 @@
+//string interpolation
+
+const name = 'Aditya'
+const count = 4 
+// console.log('Hello my name is '+ name + ' and my count is '+count); // bad way of writing as reduces the code readablity.
+// console.log(`Hello my name is ${name} and my count is ${count}`); // string interpolation -> injecting value into the string
+//this is the better way of writing
+
+const gameName = new String('Aditya-Raj') // this will create an object 
+// console.log(gameName); // we get alot of functions for strings and we can access the key value pairs of the strings
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+//and we can access all of the methods as normal
+// console.log(gameName.__proto__); //give the object
+
+//-- the best way to get to know about something is goto console of the webpage -- 
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase()); //does not change the original string
+// console.log(gameName);
+
+const newString = gameName.substring(0,4) //just postive index  -->  (0,4)=>exclusive of 4
+// console.log(newString)
+
+const anotherString = gameName.slice(-3,4) // can give negetive indexing as well
+// console.log(anotherString);
+
+
+const newStringOne = '   Aditya    '
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); //trims the extra space in the beggining
+
+
+const url = "https://aditya.com/aditya raj"
+url.replace(" ",'-') //does not modify the original string
+// console.log(url); 
+// console.log(url.replace(" ",'-'));
+
+// console.log(url.includes("aditya")); //checks weather a subtring is icnluded in the string
+// console.log(url.includes("rajeev"));
+
+console.log(gameName.split('-'))
+
+
